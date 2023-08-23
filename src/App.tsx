cee93ef60fe55290571ff1f53f593ebabf5618e4
KEITA <ksekimotonz@gmail.com>
2023-08-23 09:33:31 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>TypeScript</h1>
+      <p>リテラル型</p>
+      <p>プリミティブ型</p>
+    </>
+  )
+}
+
+export default App
